c532cf6a2cb9b202bfbc7c0ecb70ae94f7f4285e
nai <nai@nai.com>
2021-08-20 23:16:36 -0400

    Stage 4 complete

---
+++ HitNRun/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyScript : MonoBehaviour
+{
+    private float speed = 1.0f;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        //Debug.Log("Enemy spawned.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, (Vector2)player.transform.position, step);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        //Debug.Log(other.gameObject.tag);
+        if (other.gameObject == player)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
+//newEnemy.transform.position = new Vector2(Random.Range(-23f, 17f), Random.Range(-8f, 13f));
