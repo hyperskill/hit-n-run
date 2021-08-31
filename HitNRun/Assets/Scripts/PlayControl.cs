cb8cf18a2ef6f80d371b46fada5f06f348f45b79
antiusio <antoniousenko@gmail.com>
2021-08-31 17:54:49 +0300

    Stage 3 complete

--- HitNRun/Assets/Scripts/PlayControl.cs
+++ HitNRun/Assets/Scripts/PlayControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class PlayControl : MonoBehaviour
 {
     private float baseSpeed = 3.0f;
 
@@ -14,8 +14,13 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         float x = baseSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime;
         float y = baseSpeed * Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        if (transform.position.x + x > 15||transform.position.x + x < -15)
+            return;
+        if (transform.position.y + y > 15 || transform.position.y + y < -15)
+            return;
         transform.Translate(x, y, 0, Space.World);
         Camera.main.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Camera.main.transform.position.z);
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
